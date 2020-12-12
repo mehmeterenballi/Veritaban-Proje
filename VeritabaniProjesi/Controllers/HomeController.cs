9d2150587cc172022678143c91d391a52a5b0c50
mehmeterenballi <mehmeterenballi@gmail.com>
2020-12-12 14:45:51 +0300

    Database connection and ASP.Net Core MVC base
    İmportant changes and backend additions.

---
+++ VeritabaniProjesi/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using VeritabaniProjesi.Models;
+
+namespace VeritabaniProjesi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //ODBConnector.Instance.OpenConnection();
+
+            //OracleDataReader reader = ODBConnector.Instance.ReadAllFromTable("Staff");
+            //while (reader.Read())
+            //    Console.WriteLine(reader["staffname"]);
+            
+            //reader.Dispose();
+            //ODBConnector.Instance.CloseConnection();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ VeritabaniProjesi/Models/MyPosts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeritabaniProjesi.Models
+{
+    public class MyPosts
+    {
+        [Key] public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Content { get; set; }
+
+        [DataType(DataType.Currency)]
+        public int Rating { get; set; }
+    }
+}
